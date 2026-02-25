ed66fa4a1020b2077707b54b0d56a02d82d0713b
Henrique <henriqueorsoni@hotmail.com>
2026-02-25 16:16:45 -0300

    build: adicionando Procfile e configs de estáticos para o Render

--- ProjetoDjango/ProjetoDjango/settings.py
+++ ProjetoDjango/ProjetoDjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -131,3 +132,7 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # Onde o Render vai reunir os arquivos
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
